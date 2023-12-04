af2cbecaff01548fc6cec8aee63442626914f4b7
joycegill <joyceygill@gmail.com>
2023-12-04 13:58:34 -0600

    Add Comments

--- MiniProject1/CaesarCipher.java
+++ MiniProject1/CaesarCipher.java
@@ -7,7 +7,6 @@ import java.io.*;
  * 
  * @author Joyce Gill
  */
-
 public class CaesarCipher {
   public static void main(String[] args) {
     PrintWriter pen = new PrintWriter(System.out, true);
@@ -15,37 +14,46 @@ public class CaesarCipher {
     // Check that the user has entered the correct number of parameters
     if (args.length != 2) {
       pen.println("Incorrect number of parameters");
-    }
+    } // if
 
     // If arg[0] is equal to encode, encode arg[1]
     else if (args[0].equals("encode")) {
       edCaesarCipher(args[1], "encode");
-    }
+    } // else if
 
     // If arg[0] is equal to decode, decode arg[1]
     else if (args[0].equals("decode")) {
       edCaesarCipher(args[1], "decode");
-    }
+    } // else if
 
     // Check that the user has entered the correct type of parameters
     else if (!args[1].equals("encode") || !args[1].equals("decode")) {
       pen.println("Valid options are \"encode\" or \"decode\"");
-    }
+    } // else
   } // main(String[])
 
   /**
    * Helper to check that the plaintext doesn't pass the scope (lowercase letters)
+   * 
+   * @param ascii The ASCII value of a character
+   * @return The corrected ASCII value within the scope of lowercase 'a' ~ 'z'
    */
   public static char check(char ascii) {
+    // If the char is beyond the scope of lowercase 'a' ~ 'z'
     if (ascii < 'a') {
       ascii += 26;
     } else if (ascii > 'z') {
       ascii -= 26;
-    }
+    } // if/else if
     return ascii;
   } // check(char)
 
-  /** Helper to encode/decode a plaintext using Caesar Cipher */
+  /** 
+   * Helper to encode/decode a plaintext using Caesar Cipher 
+   * 
+   * @param plaintext The text to be encoded or decoded
+   * @param cipher The operation to perform - "encode" or "decode"
+  */
   public static void edCaesarCipher(String plaintext, String cipher) {
     PrintWriter pen = new PrintWriter(System.out, true);
     String newText;
--- miniProject3/README.md
+++
@@ -1,9 +0,0 @@
-## Group Members
-I worked on this project alone.
-
-## Description
-This is a program that has the ability to truncate, center, right justify, left justify, flip horizontally, and flip vertically a given TextBlock and compare it with another textblock. 
-
-## Acknowledgements
-Professor Sam Rebelsky and the labs he created for CSC 207 (The Subtype Polymorphism Lab in particular)
-StackOverflow to double check syntax
